b5622e983df954a4b1baa94c06bc652b44223f68
dagregi <120724828+dagregi@users.noreply.github.com>
2023-12-05 07:49:41 +0300

    edits were made

--- st/config.def.h
+++ st/config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+    "JetBrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*

42292d8194185e97f715fac0bff9797efb4ffc79
dagregi <120724828+dagregi@users.noreply.github.com>
2023-12-14 08:33:12 +0300

    added submodule scripts

--- bin/.local/bin/cht.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-languages=$(echo "rust typescript golang lua c python" | tr ' ' '\n')
-core_utils=$(echo "awk sed xargs" | tr ' ' '\n')
-
-selected=$(printf "$languages\n$core_utils" | fzf)
-read -p "Enter Query: " query
-
-printf $languages | grep -qs $selected && tmux neww bash -c "curl cht.sh/$selected/$(echo $query | tr ' ' '+') | less" ||
-	tmux neww bash -c "curl cht.sh/$selected~$query | less"

c5d2e952cf5c47bfab31f41d687e466df8d47160
dagregi <120724828+dagregi@users.noreply.github.com>
2023-12-21 14:14:57 +0300

    edits.......

--- config/.config/zsh/include/vcs.zsh
+++ config/.config/zsh/include/vcs.zsh
@@ -1,15 +1,15 @@
 autoload -Uz vcs_info
-zstyle ':vcs_info:git:*' formats 'on %F{black} %b%f  [ %c%u %m]'
+zstyle ':vcs_info:git:*' formats 'on %F{black} %b%f  [%c%u %m]'
 zstyle ':vcs_info:git:*' actionformats '(%F{black}%a%f)'
 zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' unstagedstr '%F{red}󰍴 %f'
+zstyle ':vcs_info:git:*' unstagedstr '%F{red}󰍴%f'
 zstyle ':vcs_info:git:*' stagedstr '%F{green} %f'
 zstyle ':vcs_info:git*+set-message:*' hooks git-st git-untracked
 
 +vi-git-untracked(){
 	[[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] &&
 	git status --porcelain | grep -q '^?? ' 2> /dev/null &&
-	hook_com[staged]+='%F{magenta}? %f'
+	hook_com[staged]+='%F{red}? %f'
 }
 +vi-git-st() {
 	local ahead behind
@@ -20,8 +20,8 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-st git-untracked
 
 	ahead=${ahead_and_behind[1]}
 	behind=${ahead_and_behind[2]}
-	(( $ahead )) && gitstatus+=( " ${ahead}" )
-	(( $behind )) && gitstatus+=( " ${behind}" )
+	(( $ahead )) && gitstatus+=( "%F{yellow} ${ahead}%f" )
+	(( $behind )) && gitstatus+=( "%F{yellow} ${behind}%f" )
 
 	hook_com[misc]+=${(j:/:)gitstatus}
 }

b51208a9de6667011dc94c05ae9ae7f38a8ec2f6
dagregi <120724828+dagregi@users.noreply.github.com>
2024-01-23 17:56:20 +0000

    bspwm refactor

--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ xbps-install -Suy >/dev/null 2>&1 &
 show_progress "Updating system" 33 $!
 install_packages base-system xorg-minimal xprop brightnessctl xclip xsetroot xf86-input-evdev xf86-video-amdgpu openssl-devel libX11-devel libXft-devel pkg-config make fontconfig-devel freetype-devel
 install_packages git tmux neovim zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp bat fd eza ripgrep fzf gnupg pass stow pulseaudio libsixel
-install_packages bspwm sxhkd rofi picom polybar feh betterlockscreen lf cava mpd mpc ncmpcpp mpv dunst newsboat htop nsxiv ffmpeg ffmpegthumbnailer zathura zathura-pdf-poppler zathura-djvu mediainfo-cli gnome-epub-thumbnailer atool odt2text
+install_packages bspwm sxhkd rofi picom polybar feh betterlockscreen lf cava mpd mpc ncmpcpp mpv dunst newsboat htop nsxiv shotgun slop ffmpeg ffmpegthumbnailer zathura zathura-pdf-poppler zathura-djvu mediainfo-cli gnome-epub-thumbnailer atool odt2text
 
 clone_repositories "git@github.com:dagregi/dotfiles.git" "git@github.com:dagregi/st.git"
 symlink_configurations

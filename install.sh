79af74ae7c15eb796c800aa86a918188690e7347
dagregi <120724828+dagregi@users.noreply.github.com>
2024-02-12 11:31:02 +0000

    changes all over the place

--- install.sh
+++ install.sh
@@ -63,13 +63,17 @@ install_fonts() {
 }
 
 clear
-sudo xbps-install -uy xbps >/dev/null 2>&1
+sudo xbps-install -Suy xbps >/dev/null 2>&1
 sudo xbps-install -Suy >/dev/null 2>&1 &
 show_progress "Updating system" 33 $!
 
-install_packages base-system xorg-minimal xprop brightnessctl xclip xsetroot xf86-input-evdev xf86-video-amdgpu openssl-devel libX11-devel libXft-devel pkg-config make fontconfig-devel freetype-devel
+install_packages base-system xorg-minimal xprop brightnessctl xclip xsetroot \
+	xf86-input-evdev mesa-dri mesa-vaapi mesa-vdpau xf86-video-amdgpu openssl-devel libX11-devel libXft-devel \
+	pkg-config make fontconfig-devel freetype-devel
 install_packages git tmux neovim zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp bat fd eza ripgrep fzf gnupg pass stow pulseaudio libsixel
-install_packages bspwm sxhkd rofi picom polybar feh betterlockscreen lf cava mpd mpc ncmpcpp mpv dunst newsboat htop nsxiv shotgun slop ffmpeg ffmpegthumbnailer zathura zathura-pdf-mupdf zathura-djvu mediainfo-cli gnome-epub-thumbnailer atool odt2text
+install_packages bspwm sxhkd rofi picom polybar feh betterlockscreen lf cava mpd \
+	mpc ncmpcpp mpv dunst newsboat htop nsxiv shotgun slop ffmpeg ffmpegthumbnailer \
+	zathura zathura-pdf-mupdf zathura-djvu mediainfo-cli gnome-epub-thumbnailer atool odt2text
 
 clone_repositories "git@github.com:dagregi/dotfiles.git" "git@github.com:dagregi/st.git"
 symlink_configurations

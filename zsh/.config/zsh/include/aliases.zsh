2549626a4c80c39fdb7ab1fcf45d4a246367f71d
dagregi <dagmawi.m@proton.me>
2025-07-04 11:23:10 +0300

    feat: initial plan to selfhost

--- zsh/.config/zsh/include/aliases.zsh
+++ zsh/.config/zsh/include/aliases.zsh
@@ -64,3 +64,5 @@ alias xin='sudo xbps-install -S $(xbps-query -Rs "" | sed "/^.\*./d" | awk "{pri
 	xup='sudo xbps-install -Su'
 # NIX
 alias update-cargo-nix='crate2nix generate && patch Cargo.nix < filter.patch && rm -f Cargo.nix.orig'
+alias open-webui='docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --rm ghcr.io/open-webui/open-webui:main'
+alias searxng='export SEARXNG_PORT=8090; docker run --rm -d -p ${SEARXNG_PORT}:8080 -v "searxng:/etc/searxng" -e BASE_URL=http://localhost:${SEARXNG_PORT} -e INSTANCE_NAME=nemesis-xng --name nemesis-xng searxng/searxng'
